1f0d9b2a72ac35793ef7132bde481d3dd1bd9d64
Paul Ssebale <32799120+paulsseb@users.noreply.github.com>
2020-02-16 22:51:14 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,18 @@
 const mongoose = require('mongoose');
 const debug = require('debug')('server');
 
-// Replace this with your MONGOURI.
-const MONGOURI = process.env.MONGODB_URI;
+//provide a sensible default for local development
+var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + 'nutritionist';
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'nutritionist';
+}
+
+
 // console.log (process.env.MONGODB_URI);
 const InitiateMongoServer = async () => {
   try {
-    await mongoose.connect(MONGOURI, {
+    await mongoose.connect(mongodb_connection_string, {
       useNewUrlParser: true
     });
     debug('Connected to DB !!');
