a919ea98a0daf32333b98a0dee4d0957f2aec536
Paul Ssebale <32799120+paulsseb@users.noreply.github.com>
2020-02-17 00:45:59 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const debug = require('debug')('server');
 
 //provide a sensible default for local development
-var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + 'nutritionist';
+var mongodb_connection_string = 'mongodb://10.129.2.45:27017/nutritionist';
 //take advantage of openshift env vars when available:
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  var mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'nutritionist';
-}
+//if(process.env.OPENSHIFT_MONGODB_DB_URL){
+ // var mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'nutritionist';
+//}
 
 
 // console.log (process.env.MONGODB_URI);
