11bce90faed60c36045bd90f7d3039c1ed744636
paulsseb <paulsseb@hotmail.com>
2020-12-05 14:48:52 +0300

    added Morgan request logger

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const app = express();
 // Configure express to handle post requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const morgan = require('morgan');
 app.use(cors());
-
+app.use(morgan('dev'));
 // Initiate Mongo Server
 InitiateMongoServer();
 
